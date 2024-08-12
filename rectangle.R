fb59aa3ccc1dbcbd68646155f1c9b99e6c0d1c42
OM_JAISWAL <omjaiswal1802@gmail.com>
2024-08-12 22:01:29 +0530

    Update and rename rectangle to rectangle.R

---
+++ rectangle.R
@@ -0,0 +1,9 @@
+rect<-function(l,b){
+ area_rect=2*(l*b)
+ print(area_rect)
+ }
+  lt<-as.integer(readline(prompt="ENTER THE LENGTH: "))
+ENTER THE LENGTH: 23
+  b<-as.integer(readline(prompt="ENTER THE BREADTH: "))
+ENTER THE BREADTH: 58
+ rect(lt,b)
